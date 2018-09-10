75e733877485e2d08dface76beeec37d7fc538ea
Bolimbo <carlo.cappellini@yahoo.com>
2018-09-09 22:23:53 -0500

    Remove method finished

--- src/com/carlocappellini/MyLinkedList.java
+++ src/com/carlocappellini/MyLinkedList.java
@@ -16,42 +16,40 @@ public class MyLinkedList implements NodeList {
     @Override
     public boolean addItem(ListItem newItem) {
 
-        if (this.root == null){
+        if (this.root == null) {
             this.root = newItem;
         }
         ListItem current = this.root;
 
 //        7   / 6
-        while (current != null){
+        while (current != null) {
 
             int comparison = current.compareTo(newItem);
-            if (comparison <0){
+            if (comparison < 0) {
                 //newItem is greater, move right
-                if (current.next() != null){
+                if (current.next() != null) {
                     current = current.next();
-                }
-                else {
+                } else {
                     // current.next is null
                     current.setNext(newItem);
                     newItem.setPrevious(current);
                 }
 
-            } else if(comparison > 0){
+            } else if (comparison > 0) {
                 // newItem is less / move left
-                if (current.previous() != null){
+                if (current.previous() != null) {
                     current.previous().setNext(newItem);
                     newItem.setPrevious(current.previous());
                     newItem.setNext(current);
                     current.setPrevious(newItem);
-                }else {
+                } else {
                     // no left link
                     newItem.setNext(this.root);
                     this.root.setPrevious(newItem);
                     this.root = newItem;
 
                 }
-            }
-            else {
+            } else {
                 System.out.println("Is already present, cannot add duplicates");
             }
 
@@ -61,7 +59,36 @@ public class MyLinkedList implements NodeList {
 
     @Override
     public boolean removeItem(ListItem item) {
+        if (item != null) {
+            System.out.println("Deleting item " + item.getValue());
+        }
+
+        ListItem currentItem = this.root;
+
+
+        while (currentItem != null) {
+            int comparison = currentItem.compareTo(item);
+            if (comparison == 0) {
+                if (currentItem == this.root) {
+                    this.root = this.root.next();
+                } else {
+                    currentItem.previous().setNext(currentItem.next());
+
+                    if (currentItem.next() != null){
+                        currentItem.next().setPrevious(currentItem.previous());
+
+                    }
+                }
+return true;
+            } else if (comparison < 0) {
+                //item to erase greater
+                currentItem = currentItem.next();
+            } else {
+                return false;
+            }
+        }
         return false;
+
     }
 
     @Override
